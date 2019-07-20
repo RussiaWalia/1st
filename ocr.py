8b1b7b9ce8d662888015735017fbe0bbc3f52e20
Juggernaut <deven@docon.co.in>
2019-07-20 00:09:17 -0700

    .place() for pixel placement, img resize

--- ocr.py
+++ ocr.py
@@ -8,59 +8,70 @@ input_=[a,a1,a2,a3]
 for x in input_:
     list0=""
     for y in x:
-        
+
         if y in "0123456789":
             list0 += y   #will keep to adding numbers to list
         if y in " ":
             print("")
-           
-            
+
+
     d= list0[0:16]  #16 digits of card
-    
+
     print(d)
-    
+
     if d[0:4]== "4000":   #condition for black and white card
         print("its a WHITE CARD")
     else:
         print("its a BLACK CARD")
-        
 
-#GUI
+
+# GUI
 
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
+
 def fill_f(path):
-    img=ImageTk.PhotoImage(Image.open(path))
+    img = ImageTk.PhotoImage(
+        Image.open(path).resize(
+            (250, 250),
+            Image.ANTIALIAS))
     Label(
         ub,
         image=img).pack()
-#Tk().update_idletasks()
     get_result()
+    ub.mainloop()
+
 
 def open_f():
-    path = filedialog.askopenfilename( initialdir="C:/Users/lenovo PC/Desktop/", title="select file")
+    path = filedialog.askopenfilename(initialdir="C:/Users/lenovo PC/Desktop/", title="select file")
     fill_f(path)
 
 
-#uloading button
-ub=Tk()    
+# uloading button
+ub = Tk()
 ub.title("FRAUD DETECTOR")
 ub.geometry("500x470")
 
 Button(
     ub,
-    text ='Upload credit card',
-    command = open_f,font=50,bg="blue",fg="white",
-    height=5, width=50).pack()        
+    text='Upload credit card',
+    command=open_f, font=50, bg="blue", fg="white",
+    height=5, width=50).pack()
 
-# FOR DISPLAYING BLACK\WHITE
+
 def get_result():
-    if d[0:4]=="4000":
-        label=Label(ub,text="WHITE CARD",fg="red",font=25).pack(side = LEFT)
-        label.pack()
-        Tk().update_idletasks()
-        
-    if d[0:4]!="4000":
-        label=Label(ub,text=" BLACK CARD").pack(side=BOTTOM)
+    if d[0:4] == "4000":
+        Label(
+            ub,
+            text="WHITE CARD",
+            fg="red", font=25).place(x=10, y=10, in_=ub)
+    if d[0:4] != "4000":
+        Label(
+            ub,
+            text="BLACK CARD").place(x=10, y=10, in_=ub)
+    ub.mainloop()
+
+
+ub.mainloop()
